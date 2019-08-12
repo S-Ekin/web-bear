acb1b8831f51fb5c078040022dd769cd991aa603
S-Ekin <sekin48111@gmail.com>
2019-08-12 09:59:50 +0800

    重新整理发布npm的配置

---
+++ src/main.tsx
@@ -0,0 +1,1 @@
+export {Button} from "@js/common/button/index";
--- typings/globals.d.ts
+++ typings/globals.d.ts
@@ -1,18 +1,5 @@
 
 
-
-interface Window{
-	__REDUX_DEVTOOLS_EXTENSION__:any;
-    hex_md5:(s:string)=>string;
-    getSession(key:"getPath"|"getUser"|"hospitalName"):any;
-   
-    sys_defaultTime:{
-        time:string,
-        rotate:number,
-    }
-}
-
-
 interface NodeModule {
         hot : {
             accept(path?: string, callback?: () => void): void
