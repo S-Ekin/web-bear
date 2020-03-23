111a09693a86c3cbf881a490a1c58f23db7dbf77
S-Ekin <sekin48111@gmail.com>
2020-03-23 18:06:34 +0800

    准备发布

---
+++ src/component/index.tsx
@@ -0,0 +1,41 @@
+import { Button, GroupBtn } from "./button/index";
+import Calendar from "./calendar";
+import { ComboTree, Combobox } from "./combo/index";
+import CrashPage from "./crashPage/CrashPage";
+import ErrorBoundary from "./crashPage/ErrorBoundary";
+import { Icon, SvgIcon, Empty } from "./icon/index";
+import { CheckBox, Input, Search } from "./input/index";
+import loadFn from "./loading/loadMethod";
+import Loading from "./loading/Loading";
+import {NavMenu} from "./menu/index";
+import Modal from "./modal";
+import Table from "./table/index";
+import notice from "./toast/index";
+import {TabList,GroupCols as ListGroup} from "./tableList/TabList";
+import {TreeTable,GroupCols as TreeGroupCols} from "./treeTable/TreeTable";
+
+export {
+  Button,
+  GroupBtn,
+  Calendar,
+  Combobox,
+  ComboTree,
+  CrashPage,
+  ErrorBoundary,
+  Icon,
+  SvgIcon,
+  Empty,
+  CheckBox,
+  Input,
+  Search,
+  Loading,
+  loadFn,
+  NavMenu,
+  Modal,
+  notice,
+  Table,
+  TabList,
+  TreeTable,
+  ListGroup,
+  TreeGroupCols
+};
